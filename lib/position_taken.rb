5b950febacfaf4af9271280704463ea72057ac1f
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-06-21 17:34:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,8 +3,10 @@ def position_taken?(board, index)
     return FALSE
   else if board[index] == ""
     return FALSE
+  else if board[index] == nil
   else if board[index] == "X" || "O"
     return TRUE
+end
 end
 end
 end
